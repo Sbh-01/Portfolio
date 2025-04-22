2a5d82f809f404f0c6b5f8f5668fe1ec45a07f36
Saurabh010312 <choubeysaurabh835@gmail.com>
2025-04-23 01:16:00 +0530

    Initial commit

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,96 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import leaf from "../../Assets/Projects/leaf.png";
+import emotion from "../../Assets/Projects/emotion.png";
+import editor from "../../Assets/Projects/codeEditor.png";
+import chatify from "../../Assets/Projects/chatify.png";
+import suicide from "../../Assets/Projects/suicide.png";
+import bitsOfCode from "../../Assets/Projects/blog.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title="EchoPrint"
+              description="A fully responsive ecommerce-like app with a beautiful UI. Users can add items to cart, view item details, see total price, and specify quantity before adding to cart. Built with React.js, Redux Toolkit for state management, React Bootstrap for styling, and React Router for routing."
+              ghLink="https://github.com/Sbh-01/Echoprint"
+              demoLink="https://echoprint.vercel.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={bitsOfCode}
+              isBlog={false}
+              title="GenPosium"
+              description="A platform to connect college students across India, promote coding contests, and encourage open-source education. GenPosium provides a space for students to find social media groups, explore national contests, and share educational resources. Built with a robust full-stack architecture using modern technologies."
+              ghLink="https://github.com/Sbh-01/GenPosium"
+              demoLink="https://genposium.onrender.com/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={editor}
+              isBlog={false}
+              title="Laravel Event Management App"
+              description="A beginner-friendly Smart Calendar and Event Management System built using Laravel. Features include task tracking, timesheet logging, calendar views, and Excel export. Supports secure authentication, email reminders, and interactive charts (pie, bar, line) for analytics. Designed with Bootstrap 5 for a clean, responsive UI."
+              ghLink="https://github.com/Sbh-01/EventDash"
+              
+            />
+          </Col>
+
+          {/* <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={leaf}
+              isBlog={false}
+              title="Plant AI"
+              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
+              ghLink="https://github.com/soumyajit4419/Plant_AI"
+              demoLink="https://plant49-ai.herokuapp.com/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={suicide}
+              isBlog={false}
+              title="Ai For Social Good"
+              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
+              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={emotion}
+              isBlog={false}
+              title="Face Recognition and Emotion Detection"
+              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
+              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
+              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+            />
+          </Col> */}
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
